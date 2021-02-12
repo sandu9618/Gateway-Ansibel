a20ba0bbcbba34953549dd05be09c84c5e2d1d85
sandu9618 <sandunipavithra9611@gmail.com>
2021-02-12 20:55:02 +0530

    first commit

---
+++ G4/collector/collector.py
@@ -0,0 +1,22 @@
+from Shell import Shell
+import threading
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from ontrafficwatch import OnTrafficWatch
+
+
+def collecting_traffic(name):
+    print(str(name)+ ' Thread starts')
+    shell = Shell()
+    shell.execute("echo \"abcd\" | sudo -S stop my")
+    shell.execute("echo \"abcd\" | sudo -S tcpdump -i any -v -G 20 not arp and not src 10.4.0.4 and not src 127.0.0.1 and not dst 10.4.0.4 and not dst 127.0.0.1 -w data-%S.pcap")
+
+def __main():
+
+    thread1 = threading.Thread(target=collecting_traffic,args=('t1', ))
+    thread1.start()
+
+    watch = OnTrafficWatch()
+    watch.run()
+if __name__ == '__main__':
+    __main()
